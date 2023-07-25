7bab949e2a095f9b8e4462e07bc0d52da5117690
Coder-Arm <anuastro002@gmail.com>
2023-07-25 21:08:05 +0530

    amend the mistakes

--- script.js
+++ script.js
@@ -1,87 +1,85 @@
-let count = 0;
-const hrs = document.getElementsByClassName('hrs');
-const mins = document.getElementsByClassName('mins');
-const secs = document.getElementsByClassName('secs');
-const setTimeBtn = document.getElementById('set-btn');
-const activeSection = document.getElementById('active-container');
+const hrs = document.getElementsByClassName("hrs");
+const mins = document.getElementsByClassName("mins");
+const secs = document.getElementsByClassName("secs");
+const setTimeBtn = document.getElementById("set-btn");
+const activeSection = document.getElementById("active-container");
 
-let countdownTimes = [];
-setTimeBtn.addEventListener('click',()=>{
-    count++;
-    clearInterval(countdownTimes[count-1]); // clear previous timer
-    countdownTimes.splice(count-1,1); 
-     if(hrs[0].value != 0 || mins[0].value != 0 || secs[0].value != 0){
-        const activeDiv = document.createElement('div');
-        activeDiv.className = "set-time-box";
-        activeDiv.innerHTML = `<span>Time Left :</span>
-        <div class="inputs">
-          <input type="number" class="hrs" placeholder="hh" />
-          <span>:</span>
-          <input type="number" class="mins" placeholder="mm" />
-          <span>:</span>
-          <input type="number" class="secs" placeholder="ss" />
-        </div>
-        <button class = "delete-btn">Delete</button>`
-        countdownTimes.push(setInterval(() => {
-            timer();
-          },1000));
-          if(activeSection.innerText === 'You have no timers currently!'){
-            activeSection.innerText = '';
-          }
-          activeSection.appendChild(activeDiv);
-     }
-     hrs[count].value = hrs[0].value;
-     mins[count].value = mins[0].value;
-     secs[count].value = secs[0].value;
-     hrs[0].value = '';
-     mins[0].value = '';
-     secs[0].value = '';
+setTimeBtn.addEventListener("click", () => {
+  if (hrs[0].value != 0 || mins[0].value != 0 || secs[0].value != 0) {
+    const activeDiv = document.createElement("div");
+    activeDiv.className = "set-time-box";
+    activeDiv.innerHTML = `<span>Time Left :</span>
+           <div class="inputs">
+             <input type="number" class="hrs" placeholder="hh" />
+             <span>:</span>
+             <input type="number" class="mins" placeholder="mm" />
+             <span>:</span>
+             <input type="number" class="secs" placeholder="ss" />
+           </div>
+           <button class = "delete-btn">Delete</button>`;
+
+        if (activeSection.innerText === "You have no timers currently!") {
+          activeSection.innerText = "";
+        }
+    activeDiv.getElementsByClassName("hrs")[0].value = hrs[0].value;
+    activeDiv.getElementsByClassName("mins")[0].value = mins[0].value;
+    activeDiv.getElementsByClassName("secs")[0].value = secs[0].value;
+
+    activeSection.appendChild(activeDiv);
+    timer(activeDiv, hrs[0].value, mins[0].value, secs[0].value);
+    hrs[0].value = "";
+    mins[0].value = "";
+    secs[0].value = "";
+  }
 });
 
-function timer(){
-    if(hrs[count].value == 0 && mins[count].value == 0 && secs[count].value == 0){
-        hrs[count].value = '';
-        mins[count].value = '';
-        secs[count].value = '';
-       clearInterval(countdownTimes[count-1]);
-    //    countdownTimes.splice(count-1,1)
-       timesUp();
+function timer(element, hrs, mins, secs) {
+  const interval = setInterval(() => {
+    if (hrs == 0 && mins == 0 && secs == 0) {
+      hrs = "";
+      mins = "";
+      secs = "";
+      clearInterval(interval);
+      timesUp(element);
+    } else if (secs > 59) {
+      const times = Math.floor(secs / 60);
+      mins = mins + times;
+      secs -= 60 * times;
+    } else if (secs != 0) {
+      secs--;
+    } else if (mins > 59 && secs == 0) {
+      const times = Math.floor(mins / 60);
+      hrs = hrs + times;
+      mins -= 60 * times;
+    } else if (mins != 0 && secs == 0) {
+      mins--;
+      secs = 59;
+    } else if (hrs != 0 && mins == 0) {
+      hrs--;
+      mins = 59;
+      secs = 59;
     }
-    else if(secs[count].value > 59){
-        const times = Math.floor(secs[count].value/60);
-        mins[count].value = mins[count].value + times;
-        secs[count].value -= (60*times);
-    }
-    else if (secs[count].value != 0){
-        secs[count].value--;
-    }
-    else if(mins[count].value > 59 && secs[count].value == 0){
-        const times = Math.floor(mins[count].value/60);
-        hrs[count].value = hrs[count].value + times;
-        mins[count].value -= (60*times);
-    }
-    else if(mins[count].value != 0 && secs[count].value == 0){
-        mins[count].value--;
-        secs[count].value = 59;
-    }
-    else if(hrs[count].value != 0 && mins[count].value == 0){
-        hrs[count].value--;
-        mins[count].value = 59;
-        secs[count].value = 59;
-    }
+      
+    element.getElementsByClassName("hrs")[0].value = hrs;
+    element.getElementsByClassName("mins")[0].value = mins;
+    element.getElementsByClassName("secs")[0].value = secs;
+  }, 1000);
+
+  element.getElementsByClassName("delete-btn")[0].addEventListener('click',()=>{
+    element.remove();
+  })
 }
 
-function timesUp(){
-   let currDiv =  document.getElementsByClassName('set-time-box')[count];
-   currDiv.style.display = "none";
-   const activeDiv = document.createElement("div");
-   activeDiv.className ='set-time-box timesUpBox';
-   activeDiv.innerHTML = `<audio class= "audio" src="./mixkit-happy-bells-notification-937.wav" loop ></audio><span class="timesUpText">Time is Up !</span><button id="stop-btn">Stop</button>`
-   activeSection.appendChild(activeDiv);
-   const audioElement = activeDiv.querySelector('.audio');
-   audioElement.play(); 
-   document.getElementById("stop-btn").addEventListener("click",()=>{
+function timesUp(element) {
+
+  element.style.display = "none";
+  const activeDiv = document.createElement("div");
+  activeDiv.className = "set-time-box timesUpBox";
+  activeDiv.innerHTML = `<audio class= "audio" src="./mixkit-happy-bells-notification-937.wav" loop ></audio><span class="timesUpText">Time is Up !</span><button id="stop-btn">Stop</button>`;
+  activeSection.appendChild(activeDiv);
+  const audioElement = activeDiv.querySelector(".audio");
+  audioElement.play();
+  document.getElementById("stop-btn").addEventListener("click", () => {
     activeDiv.remove();
-   })
+  });
 }
-    
